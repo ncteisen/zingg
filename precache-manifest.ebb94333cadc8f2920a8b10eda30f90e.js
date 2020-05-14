de8be30900a70cced13f5e3af2c0f5c5bdd6248d
ncteisen <ncteisen@google.com>
2020-05-14 14:09:38 -0700

    Updates

---
+++ precache-manifest.ebb94333cadc8f2920a8b10eda30f90e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240db589059dee1e8eb1a380d6b39505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e824a81770613f6aa98",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0024154651f1a132c312",
+    "url": "/static/css/main.0563f177.chunk.css"
+  },
+  {
+    "revision": "4e824a81770613f6aa98",
+    "url": "/static/js/2.eb09e40b.chunk.js"
+  },
+  {
+    "revision": "e8c1be7651174d04e147076195f059fa",
+    "url": "/static/js/2.eb09e40b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0024154651f1a132c312",
+    "url": "/static/js/main.086f8297.chunk.js"
+  },
+  {
+    "revision": "36f48cdc6241f6f57c49",
+    "url": "/static/js/runtime-main.7ec330c8.js"
+  },
+  {
+    "revision": "0b5e90ff78458abbfc402b70e616ff92",
+    "url": "/static/media/alcoholism.0b5e90ff.png"
+  },
+  {
+    "revision": "e37c667d8810c82251b7be081396fa52",
+    "url": "/static/media/back.e37c667d.png"
+  },
+  {
+    "revision": "cbb90e8dbdf7477e370a2b68865a007b",
+    "url": "/static/media/beer1.cbb90e8d.png"
+  },
+  {
+    "revision": "5617219e9f9b82e353d242f42f7b2262",
+    "url": "/static/media/cake.5617219e.png"
+  },
+  {
+    "revision": "691ae4c642f49d7115f2fd82d5c0bfc6",
+    "url": "/static/media/categories.691ae4c6.png"
+  },
+  {
+    "revision": "2c16cdf8b50935d4c4483e3c193a3235",
+    "url": "/static/media/chug.2c16cdf8.png"
+  },
+  {
+    "revision": "72936340679763ad06c8c72d8689e23e",
+    "url": "/static/media/drink1.72936340.png"
+  },
+  {
+    "revision": "9672389d114dbd4574ab84f91d1065f8",
+    "url": "/static/media/force.9672389d.png"
+  },
+  {
+    "revision": "2da4573f40b093e1f7c4c9355de482cb",
+    "url": "/static/media/hotseat2.2da4573f.png"
+  },
+  {
+    "revision": "abb60a92446460ce2df67061c5bf7444",
+    "url": "/static/media/marriage.abb60a92.png"
+  },
+  {
+    "revision": "363f54499f715aea2ae3e6d1d5bd9510",
+    "url": "/static/media/movie1.363f5449.png"
+  },
+  {
+    "revision": "bcfe1429e24ff318318785b22c2b3192",
+    "url": "/static/media/noah.bcfe1429.png"
+  },
+  {
+    "revision": "1063ad6edc9cdb46d57e445b56cc28bd",
+    "url": "/static/media/orator.1063ad6e.png"
+  },
+  {
+    "revision": "8fc79f745e77bea1168edc600f1b17eb",
+    "url": "/static/media/quiet.8fc79f74.png"
+  },
+  {
+    "revision": "52fcd7693f6bce164dc3a261fdf4cc23",
+    "url": "/static/media/races.52fcd769.png"
+  },
+  {
+    "revision": "5fc4d3cbe19c42968470e9c81c21473f",
+    "url": "/static/media/sleuth.5fc4d3cb.png"
+  },
+  {
+    "revision": "fd941560f34fec59c15515f8f7f75af5",
+    "url": "/static/media/sponge.fd941560.png"
+  },
+  {
+    "revision": "361fa2028dc475f80fe53cb612fcc113",
+    "url": "/static/media/spy.361fa202.png"
+  },
+  {
+    "revision": "055e562d6736849706ffc9ba26318bee",
+    "url": "/static/media/viking.055e562d.png"
+  }
+]);
